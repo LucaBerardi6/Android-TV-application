cdfc7edf7d372b3f15cd6c805faac2be124222a6
LucaBerardi6 <luca.berardi6@studio.unibo.it>
2022-11-19 19:32:23 +0100

    game experience improvements

--- AndroidTVApp/app/src/main/java/com/example/lucaberardi/QuestionsList.java
+++ AndroidTVApp/app/src/main/java/com/example/lucaberardi/QuestionsList.java
@@ -6,7 +6,29 @@ import java.util.Random;
 
 public class QuestionsList {
 
+    /**questions that will be shown*/
     private static String questions[] = {
+            "When was Android TV officially presented?",
+            "What is man's best friend?",
+            "By whom was the Mona Lisa painted?",
+            "Who is the protagonist of the Dragon Ball series?",
+            "When did World War II start?",
+            "What is the capital of Italy?",
+            "When was America discovered?",
+            "What is the capital of Spain?",
+            "In which city is the Teatro alla Scala located?",
+            "Who is Leopardi?",
+            "In which of these sports is a racket used?",
+            "What is the equivalent of one meter?",
+            "2+(3*5)=?",
+            "Where is the Colosseum located?",
+            "By whom was the Ultima Cena painted?",
+            "How many Europeans has the Italian national team won until 2021?"
+
+    };
+
+    /** Italian version of the questions*/
+    /* private static String questions[] = {
             "Quando è stato presentato ufficialmente Android TV?",
             "Qual è il miglior amico dell'uomo?",
             "Da chi è stata dipinta la Gioconda?",
@@ -24,8 +46,67 @@ public class QuestionsList {
             "Da chi è stata dipinta l'Ultima Cena?",
             "Quanti europei ha vinto la nazionale italiana?"
 
+    };*/
+    private static  String correctAnswer[] = {
+            "2014",
+            "Dog",
+            "Da Vinci",
+            "Goku",
+            "1914",
+            "Rome",
+            "1492",
+            "Madrid",
+            "Milan",
+            "A poet",
+            "Tennis",
+            "100 cm",
+            "17",
+            "Rome",
+            "Da Vinci",
+            "2"
+
     };
 
+    private static String answer2[] = {
+            "2010",
+            "cat",
+            "Giotto",
+            "Vegeta",
+            "1918",
+            "London",
+            "1500",
+            "Barcelona",
+            "Rome",
+            "A painter",
+            "Swimming",
+            "1000 cm",
+            "100",
+            "Naples",
+            "Van Gogh",
+            "3"
+    };
+
+    private static  String answer3[] = {
+            "2012",
+            "Rabbit",
+            "Picasso",
+            "Ghoan",
+            "1920",
+            "Milan",
+            "1592",
+            "Valencia",
+            "Turin",
+            "A football player",
+            "Football",
+            "100 mm",
+            "18",
+            "Bologna",
+            "Monet",
+            "1"
+    };
+
+    /** Italian version of the  answer.*/
+    /*
     private static  String correctAnswer[] = {
             "2014",
             "Cane",
@@ -83,38 +164,38 @@ public class QuestionsList {
             "Monet",
             "1"
     };
-
-
+*/
+    /**Returns a random list of questions with their answers.*/
     public static List<Question> getQuestionList(int number) {
 
         List<Question> list = new ArrayList<>();
 
         Random random= new Random();
         Random random2= new Random();
-        List<Integer> duplicate =  new ArrayList<>(); // per verificare che non escano domande uguali in una partita
+        List<Integer> duplicate =  new ArrayList<>(); // to verify that no identical questions come up in a game
         int next;
 
         for (int index = 0; index < number; index++) {
 
-            next= random.nextInt(questions.length); //numero per prendere una domanda casuale
-            //domande sempre diverse:
+            next= random.nextInt(questions.length); // number to take a random question
+            //always different questions:
             while(duplicate.contains(next))  next=random.nextInt(questions.length);
             duplicate.add(next);
             //
-            //PRENDE 3 RISPOSTE CASUALI:
+            //TAKES 3 RANDOM ANSWERS:
             int r1= random2.nextInt(3);
             int r2= random2.nextInt(3);
             int r3= random2.nextInt(3);
             while(r2==r1) r2= random2.nextInt(3);
             while(r3==r1 || r3==r2) r3= random2.nextInt(3);
-
-            //MISCHIA LE POSSIBILI RISPOSTE:
+            //
+            //MIX THE POSSIBLE ANSWERS:
             String [] randomQuestion = new String[3];
             randomQuestion[r1]=correctAnswer[next];
             randomQuestion[r2]=answer2[next];
             randomQuestion[r3]=answer3[next];
 
-            //CREA NUOVA DOMANDA:
+            //CREATE NEW QUESTION:
             Question question = new Question(questions[next],correctAnswer[next],randomQuestion[0],
                                                 randomQuestion[2],randomQuestion[1]);
             list.add(question);
